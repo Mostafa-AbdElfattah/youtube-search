59140dd092d428a9bded715c1bcff14d2c4a8b36
TaAaAa7 <mostafa.ahmed.abdelfattah@gmail.com>
2020-12-13 23:02:05 +0000

    new version

---
+++ precache-manifest.b18a90f6e25053758ea460e344fb2808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb52ac9f80f67dc3afb9ffc7d69213e",
+    "url": "/youtube-search/index.html"
+  },
+  {
+    "revision": "a4c95de93aa7b61e88ae",
+    "url": "/youtube-search/static/css/main.46f90d43.chunk.css"
+  },
+  {
+    "revision": "c8c5862932cdb75d545f",
+    "url": "/youtube-search/static/js/2.bc27dfc2.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube-search/static/js/2.bc27dfc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c95de93aa7b61e88ae",
+    "url": "/youtube-search/static/js/main.ded6903f.chunk.js"
+  },
+  {
+    "revision": "4b2de9eab431041b0c6f",
+    "url": "/youtube-search/static/js/runtime-main.c40eca9c.js"
+  }
+]);
